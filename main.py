f3a603ac54f3dc9382743a8b68a1dd8f6626799d
alistair-lilley <jklilley12@gmail.com>
2023-06-02 21:42:34 -0700

    fix1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import discord
+import argparse
 
 from dotenv import dotenv_values
 
@@ -8,6 +9,9 @@ from botobjs.tginter import TGInterface
 from dbobjs.database import Database
 
 from constants import BULK_DATA_URL, RULES_URL
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--clear', action="store_true")
 
 config = dotenv_values(".env")
 
@@ -20,6 +24,10 @@ intents.messages = True
 intents.message_content = True
 
 if __name__=="__main__":
+    args = parser.parse_args()
+    database = Database(BULK_DATA_URL, RULES_URL)
+    if args.clear:
+        database.clear_data()
     database = Database(BULK_DATA_URL, RULES_URL)
     dcbot = DCInterface(database, intents=intents)
     tgbot = TGInterface(TGTOKEN, database)
